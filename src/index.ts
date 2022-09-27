647bc1c31c8bdd6a0c839233317808c6d1bcf94d
GuiCoelho-S <gs.coelho@outlook.com>
2022-09-27 00:28:04 -0300

    feat: add app.use and cors. Create interface to post health Insurance

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
+import cors from "cors";
+import healthInsurance from "./services/firebase";
+
 import { Request, Response } from "express";
+interface IHealthInsurance {
+  name: string;
+  cnpj: string;
+  doctors: { name: string; id: number }[] | [];
+}
+
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   console.log("App is running");
 
   return res.json({ message: "Hello world!" });
+});
+
+app.post("/health-insurance", async (req: Request, res: Response) => {
+  const data = req.body as IHealthInsurance;
+
+  await healthInsurance.add(data);
+
+  res.send({ message: "Health Insurance has created!" });
 });
 
 app.listen(8080);
