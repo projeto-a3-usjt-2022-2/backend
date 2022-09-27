67c2234b399e6cf6ab82dc85060be2df476ba92d
GuiCoelho-S <gs.coelho@outlook.com>
2022-09-27 00:28:22 -0300

    feat: create dataBase firebase config

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+const firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+firebase.intializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+const healthInsurance = db.collection("health_insurance");
+
+export default healthInsurance;
