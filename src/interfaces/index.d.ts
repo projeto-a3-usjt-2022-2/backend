d614482a8c1eb77656b5f0a04b4a55241e67654d
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-02 19:55:36 -0300

    feat: add userInterface to help development workflow

---
+++ src/interfaces/index.d.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  name: string;
+  lastName: string;
+  birthDate: string; // needs to be into toISOString;
+  sex: string;
+  cpf: string;
+  crm: null | string;
+  email: string;
+  password: string;
+}
