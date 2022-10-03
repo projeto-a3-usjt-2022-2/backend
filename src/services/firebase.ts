5eba71601bb3bbb598d66ddaafe847aa245ca11b
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-02 22:48:57 -0300

    feat: create default firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,19 +1,9 @@
-const firebase = require("firebase");
+import * as admin from "firebase-admin";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-};
+const firebaseConfig = require("../../firebase_credentials.json");
 
-firebase.intializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig),
+});
 
-const db = firebase.firestore();
-
-const healthInsurance = db.collection("health_insurance");
-
-export default healthInsurance;
+export const db = admin.firestore();
