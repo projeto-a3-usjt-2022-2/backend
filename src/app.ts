ed02be1075b779a90ae8a13da2acd6be8f348200
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-02 22:48:34 -0300

    feat: change port to test development

--- src/app.ts
+++ src/app.ts
@@ -8,4 +8,4 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(8082, () => console.log("Server is running on port 8082"));
+app.listen(8080, () => console.log("Server is running on port 8080"));
