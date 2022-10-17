10073db209207edf5d303e3969eddf114d6af802
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-17 19:55:10 -0300

    feat: change port

--- src/app.ts
+++ src/app.ts
@@ -8,4 +8,4 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(8080, () => console.log("Server is running on port 8080"));
+app.listen(8081, () => console.log("Server is running on port 8081"));
