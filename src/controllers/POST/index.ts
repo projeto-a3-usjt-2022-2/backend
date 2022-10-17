6125cb7eb74e9bbaba9b7b3f13a85faffef74447
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-17 19:54:29 -0300

    feat: create logIn doc

--- src/controllers/POST/index.ts
+++ src/controllers/POST/index.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
 import { IConsult, IUser } from "../../interfaces";
 import { db } from "../../services/firebase";
+
 const avaliableClinics = ["Albert Einstein Morumbi"];
+
 export const createUser = async (request: Request, response: Response) => {
   const user = request.body as IUser;
 
+  if (!avaliableClinics.includes(user.clinic))
+    return response.status(400).json({
+      error: "Clinic is not registrated",
+      avaliableClinics: avaliableClinics,
+    });
   const clinicRef = db.collection("Clinics").doc(user.clinic);
 
   let usersRef;
@@ -19,7 +26,18 @@ export const createUser = async (request: Request, response: Response) => {
 
   await usersRef
     .add({ ...user, createdAt })
-    .then(async () => {
+    .then(async ({ id }) => {
+      console.log("this is data", id);
+
+      await db
+        .collection("LogIn")
+        .doc(id)
+        .set({ email: user.email, id, cpf: user.cpf })
+        .catch((error) => {
+          return response
+            .status(400)
+            .json({ error: `An error has ocurried: ${error}` });
+        });
       return response.status(200).json({
         message: "User created successfully!",
       });
