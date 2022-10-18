758771c395aa6c037b386bd1670b0432f0181f90
GuiCoelho-S <gs.coelho@outlook.com>
2022-10-17 21:25:49 -0300

    feat: create login route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { getUser } from "../controllers/GET";
-import { createConsult, createUser } from "../controllers/POST";
+import {
+  createConsult,
+  createUser,
+  verifyUserLogin,
+} from "../controllers/POST";
 import { validatedConsult, validatedUser } from "../middlewares";
 
 export const router = express.Router();
@@ -8,5 +12,5 @@ export const router = express.Router();
 router.get("/users", getUser);
 
 router.post("/createUser", validatedUser, createUser);
-
 router.post("/createConsult", validatedConsult, createConsult);
+router.post("/login", verifyUserLogin);
