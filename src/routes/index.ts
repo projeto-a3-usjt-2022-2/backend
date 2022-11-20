bc31ef7848163caff50e1c445446faac690129c9
guicoelhodev <gs.coelho_dev@outlook.com>
2022-11-19 22:45:06 -0300

    feat: add doctorSchedule and consults route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { getUser } from "../controllers/GET";
+import { avaliableSchedule } from "../controllers/POST/avaliableSchedule";
 import { getDoctorsByClinic } from "../controllers/GET/getDoctor";
 import {
   createConsult,
   createUser,
   verifyUserLogin,
 } from "../controllers/POST/_index";
-import { validatedConsult, validatedUser } from "../middlewares";
+import {
+  validatedConsult,
+  validatedUser,
+  validateGetConsults,
+} from "../middlewares";
+import { allConsultsById } from "../controllers/POST/allConsultsById";
 
 export const router = express.Router();
 
@@ -16,3 +22,5 @@ router.get("/doctors", getDoctorsByClinic);
 router.post("/createUser", validatedUser, createUser);
 router.post("/createConsult", validatedConsult, createConsult);
 router.post("/login", verifyUserLogin);
+router.post("/doctorSchedule", avaliableSchedule);
+router.post("/consults", validateGetConsults, allConsultsById);
