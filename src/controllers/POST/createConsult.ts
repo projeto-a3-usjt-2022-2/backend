7d70b1ba6a762f81360f5ad942473eb2d317fe13
guicoelhodev <gs.coelho_dev@outlook.com>
2022-11-19 22:43:37 -0300

    refactor: consult route to get based on schedule object key

--- src/controllers/POST/createConsult.ts
+++ src/controllers/POST/createConsult.ts
@@ -4,48 +4,90 @@ import { IConsult } from "../../interfaces";
 import { db } from "../../services/firebase";
 
 export const createConsult = async (request: Request, response: Response) => {
-    const consultInfo = request.body as IConsult;
+  const consultInfo = request.body as IConsult;
 
-    if (!avaliableClinics.includes(consultInfo.clinic))
-        return response.status(400).json({
-            error: "Clinic is not registrated",
-            avaliableClinics: avaliableClinics,
-        });
+  console.log("consult info", consultInfo);
 
-    const clinicsRef = db
-        .collection("Consults")
-        .doc(consultInfo.clinic)
-        .collection("Consults");
+  const clinicRef = db.collection("Clinics").doc(consultInfo.clinic);
 
-    clinicsRef
-        .add(consultInfo)
-        .then(() => console.log("Consult created successfully"))
-        .catch(() =>
-            response.status(400).json({
-                error: "Something went wrong!",
-            })
-        );
+  const { cep } = (await clinicRef.get()).data() as any;
 
-    const userId = consultInfo.user.id;
-
-    const usersRef = db
-        .collection("Clinics")
-        .doc(consultInfo.clinic)
-        .collection("Users");
-
-    const currentUser = usersRef.doc(userId);
-    let tmpConsult = {
-        date: consultInfo.date,
-        hour: consultInfo.hour,
-        modality: consultInfo.modality,
-    };
-
-    let currentUserData = (await currentUser.get()).data();
-
-    currentUser.update({
-        ...currentUserData,
-        consults: [...currentUserData?.consults, tmpConsult],
+  if (!avaliableClinics.includes(consultInfo.clinic))
+    return response.status(400).json({
+      error: "Clinic is not registrated",
+      avaliableClinics: avaliableClinics,
     });
 
-    return response.status(200);
+  const clinicsRef = db
+    .collection("Consults")
+    .doc(consultInfo.clinic)
+    .collection("Consults");
+
+  let doc = (await clinicsRef.doc(consultInfo.date).get()).data() as
+    | {
+        schedule: IConsult[] | [];
+      }
+    | undefined;
+
+  // Add date in each user
+  let userRef = db
+    .collection("Clinics")
+    .doc(consultInfo.clinic)
+    .collection("Users")
+    .doc(consultInfo.userId);
+
+  let doctorRef = db
+    .collection("Clinics")
+    .doc(consultInfo.clinic)
+    .collection("Doctors")
+    .doc(consultInfo.doctorId);
+
+  let userInfo = (await userRef.get()).data();
+  let doctorInfo = (await doctorRef.get()).data();
+
+  if (!doc) {
+    clinicsRef.doc(consultInfo.date).set({
+      schedule: [{ ...consultInfo, status: "waiting", cep: cep }],
+    });
+  } else if (doc && doc.schedule.length === 0) {
+    await clinicsRef
+      .doc(consultInfo.date)
+      .set({ schedule: [{ ...consultInfo, status: "waiting", cep: cep }] })
+      .catch(() =>
+        response.status(401).json({ message: "Something went wrong" })
+      );
+  } else {
+    let tmpSchedule = doc.schedule;
+
+    await clinicsRef
+      .doc(consultInfo.date)
+      .set({
+        schedule: [
+          ...tmpSchedule,
+          { ...consultInfo, status: "waiting", cep: cep },
+        ],
+      })
+      .catch(() =>
+        response.status(401).json({ message: "Something went wrong" })
+      );
+  }
+  // when consult was created, add a ref into Users and Doctor collections
+
+  addNewDateIntoSchedule(doctorRef, doctorInfo, consultInfo.date);
+  addNewDateIntoSchedule(userRef, userInfo, consultInfo.date);
+
+  return response
+    .status(200)
+    .json({ data: "consult was created successfully" });
+};
+
+const addNewDateIntoSchedule = (userRef: any, userInfo: any, date: string) => {
+  let tmpSchedule = userInfo.schedule as string[] | undefined;
+
+  if (!tmpSchedule) return userRef.set({ schedule: [date] }, { merge: true });
+
+  if (!tmpSchedule.some((item) => item === date)) {
+    tmpSchedule = [...tmpSchedule, date];
+    userRef.set({ schedule: tmpSchedule }, { merge: true });
+  }
 };
